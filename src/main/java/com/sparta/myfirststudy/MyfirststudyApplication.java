871652f9520b36fd9df3ea2c9e997f68300ab9dd
SJY <bangstation@naver.com>
2024-10-17 12:00:44 +0900

    배포용 도커파일 포함

---
+++ src/main/java/com/sparta/myfirststudy/MyfirststudyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myfirststudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirststudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirststudyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sparta/myfirststudy/controller/MyfirststudyController.java
@@ -0,0 +1,32 @@
+package com.sparta.myfirststudy.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Controller
+public class MyfirststudyController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyfirststudyController.class);
+
+    @GetMapping("/helloworld")
+    public String Myfirststudy(Locale locale, Model model) {
+        logger.info("Welcome home! The client locale is {}.", locale);
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateFormat.format(date);
+
+        // model 객체에 서버 시간을 추가
+        model.addAttribute("serverTime", formattedDate);
+
+        // 뷰 파일 이름을 반환 (리디렉션 대신)
+        return "myfirststudy"; // templates/myfirststudy.html 파일을 가리킴
+    }
+}
